8bf2fed3bf757394fa346fc5d3b53c18c85f235a
Drought-Ender <ethanmucciarone@icloud.com>
2023-12-27 14:29:15 -0600

    AAAAAAAAAAA CRASH

--- include/PSGame/PikScene.h
+++ include/PSGame/PikScene.h
@@ -36,25 +36,30 @@ struct PikScene : public PSSystem::Scene {
  * @size{0xD}
  */
 struct PikSceneMgr : public PSSystem::SceneMgr {
-	virtual void newAndSetCurrentScene(SceneInfo&);                    // _0C
-	virtual void newAndSetGlobalScene();                               // _10
-	virtual void newMainBgm(const char*, JAInter::SoundInfo&)     = 0; // _14
-	virtual void newDirectedBgm(const char*, JAInter::SoundInfo&) = 0; // _18
-	virtual void newAutoBgm(const char*, const char*, JAInter::SoundInfo&, JADUtility::AccessMode, SceneInfo&,
-	                        PSSystem::DirectorMgrBase*)
-	    = 0;                                       // _1C
-	virtual void curSceneIsBigBossFloor();         // _20 (weak)
-	virtual void newGameScene(u8, SceneInfo*) = 0; // _24
+	PikSceneMgr()
+	    : mAccessMode(0)
+	{
+	}
+
+	virtual PSSystem::Scene* newAndSetCurrentScene(SceneInfo&);                     // _0C
+	virtual PSSystem::Scene* newAndSetGlobalScene();                                // _10
+	virtual PSSystem::BgmSeq* newMainBgm(const char*, JAInter::SoundInfo&)     = 0; // _14
+	virtual PSSystem::BgmSeq* newDirectedBgm(const char*, JAInter::SoundInfo&) = 0; // _18
+	virtual PSSystem::BgmSeq* newAutoBgm(const char*, const char*, JAInter::SoundInfo&, JADUtility::AccessMode, SceneInfo&,
+	                                     PSSystem::DirectorMgrBase*)
+	    = 0;                                                   // _1C
+	virtual bool curSceneIsBigBossFloor() { return false; }    // _20 (weak)
+	virtual PSSystem::Scene* newGameScene(u8, SceneInfo*) = 0; // _24
 
 	PSSystem::BgmSeq* newBgmSeq(const char*, JAInter::SoundInfo&);
 	PSSystem::BgmSeq* newStreamBgm(u32, JAInter::SoundInfo&);
-	void initBossBgm(SceneInfo&, u8*);
+	PSSystem::BgmSeq* initBossBgm(SceneInfo&, u8*);
 	void initAdditionalBgm(SceneInfo&, PSSystem::Scene*);
-	void initMainBgm(SceneInfo&, u8*);
+	PSSystem::BgmSeq* initMainBgm(SceneInfo&, u8*);
 
 	// _00      = VTBL
 	// _00-_08  = SceneMgr
-	u8 _0C; // _0C, unknown
+	u8 mAccessMode; // _0C,  JADUtility::AccessMode
 };
 } // namespace PSGame
 
--- src/DroughtMod/yakushimaMusic.cpp
+++ src/DroughtMod/yakushimaMusic.cpp
@@ -1,4 +1,5 @@
 #include "PSGame/PikScene.h"
+#include "PSSystem/PSMainSide_Scene.h"
 
 namespace PSGame
 {
@@ -6,6 +7,8 @@ namespace PSGame
 // useYakushima4__Q26PSGame13CaveFloorInfoFv
 bool CaveFloorInfo::useYakushima4() {
     OSReport("%p CaveFloorInfo::useYakushima4()\n", this);
+    OSReport("I HATE PIK2 SOUND I HATE\n");
+
     if (isCaveFloor()) {
         if (mSceneType == SceneInfo::COURSE_YAKUSHIMA) {
             if (getCaveNoFromID() == 3 && !isBossFloor()) {
@@ -14,6 +17,7 @@ bool CaveFloorInfo::useYakushima4() {
         }
         else if (mSceneType == SceneInfo::COURSE_LAST) {
             if (getCaveNoFromID() == 3 && mFloorNum == 29) {
+                OSReport("True!\n");
                 return true;
             }
         }
@@ -21,4 +25,35 @@ bool CaveFloorInfo::useYakushima4() {
     return false;
 }
 
+
+
 } // namespace PSGame
+
+namespace PSM
+{
+
+void Scene_Game::init()
+{
+	static_cast<SceneMgr*>(PSMGetSceneMgrCheck())->initEnvironmentSe(this);
+
+	if (needBossBgm()) {
+		attachBossFaderMgr();
+	}
+}
+
+void Scene_Cave::init()
+{
+	if (isBossFloor()) {
+		static_cast<SceneMgr*>(PSMGetSceneMgrCheck())->initEnvironmentSe(this);
+
+		if (needBossBgm()) {
+			mBossFaderMgr = PSSystem::SingletonBase<BossBgmFader::Mgr>::getInstance();
+		}
+		mBossFaderMgr = nullptr; // so much for what we just did?
+		return;
+	}
+
+	Scene_Game::init();
+}
+
+} // namespace PSSystem
