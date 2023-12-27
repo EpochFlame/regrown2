40f4e61b08586cbf624d16eb8fc5901f56240b26
Drought-Ender <ethanmucciarone@icloud.com>
2023-12-27 12:35:06 -0600

    Rebase & fix crash
    Note: Crashing wistful wild due to someone else's code. Whomever's fault this is: FIX IT.

--- src/DroughtMod/yakushimaMusic.cpp
+++ src/DroughtMod/yakushimaMusic.cpp
@@ -5,16 +5,20 @@ namespace PSGame
 
 // useYakushima4__Q26PSGame13CaveFloorInfoFv
 bool CaveFloorInfo::useYakushima4() {
-    if (isCaveFloor() && mSceneType == SceneInfo::COURSE_YAKUSHIMA) {
-		if (getCaveNoFromID() == 3 && !isBossFloor()) {
-            return true;
+    OSReport("%p CaveFloorInfo::useYakushima4()\n", this);
+    if (isCaveFloor()) {
+        if (mSceneType == SceneInfo::COURSE_YAKUSHIMA) {
+            if (getCaveNoFromID() == 3 && !isBossFloor()) {
+                return true;
+            }
+        }
+        else if (mSceneType == SceneInfo::COURSE_LAST) {
+            if (getCaveNoFromID() == 3 && mFloorNum == 29) {
+                return true;
+            }
         }
     }
-    if (isCaveFloor() && mSceneType == SceneInfo::COURSE_LAST) {
-        if (getCaveNoFromID() == 3 && mFloorNum == 29) {
-            return true;
-        }
-    }
+    return false;
 }
 
 } // namespace PSGame
